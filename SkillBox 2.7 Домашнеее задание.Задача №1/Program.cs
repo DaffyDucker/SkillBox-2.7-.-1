c1f25fb3418bb8ab3b77b949011a92edf693e132
DaffyDuck <DaffyDuck@DESKTOP-NEF26NU>
2021-10-28 09:05:01 +0300

    Добавьте файлы проекта.

---
+++ SkillBox 2.7 Домашнеее задание.Задача №1/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SkillBox_2._7_Домашнеее_задание.Задача__1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string FullName = "Иван Иванович Иванов";
+            int age = 22;
+            string email = "test@test.com";
+            double ScoreProgramming = 59.84;
+            double ScoreMath = 89.7;
+            double ScorePhisics = 90.6;
+
+            Console.WriteLine($"Ф.И.О.: {FullName}" +
+                              $"\nВозраст: {age} " +
+                              $"\nEmail: {email}" +
+                              $"\nБаллы по программированию: {ScoreProgramming}" +
+                              $"\nБаллы по математике: {ScoreMath}" +
+                              $"\nБаллы по физике: {ScorePhisics}");
+        }
+    }
+}
